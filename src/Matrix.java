78b07ce6b0f7bc4c2caa8ee8dc398baefb99c694
Bitha17 <16521076@mahasiswa.itb.ac.id>
2022-09-20 22:59:51 +0700

    day2

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.*;
+// import java.lang.Math;
+
 public class Matrix {
     private int row, col;
     private double[][] contents;
@@ -30,12 +34,125 @@ public class Matrix {
         return this.col;
     }
 
-    /* *** READ AND DISPLAY *** */
+    /* *** READ, DISPLAY, SAVE TO FILE *** */
+    void readMatrix() {
+    /* Membaca matrix dari input keyboard */
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < this.row; i++){
+            for (int j = 0; j < this.col; j++){
+                double x = in.nextDouble();
+                setELMT(x, row, col);
+            }
+        }
+        in.close();
+    }
 
+    void readMatrix2(File text) {
+    /* Membaca matrix dari file */
+        try { 
+            Scanner in = new Scanner(text);
+            for (int i = 0; i < this.row; i++){
+                for (int j = 0; j < this.col; j++){
+                    double x = in.nextDouble();
+                    setELMT(x, row, col);
+                }
+            }
+            in.close();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void displayMatrix() {
+    /* Menampilkan matrix ke layar */
+        for (int i = 0; i < this.row; i++){
+            for (int j = 0; j < this.col; j++){
+                System.out.print(this.contents[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void displaySPL() {
+    /* Menampilkan matrix penyelesaian SPL  */
+        for (int i = 1; i <= this.row; i++){
+            System.out.println("X" + i + "=" + this.contents[i-1][0]);
+        }
+    }
+
+    void matrixToFile(File output) {
+    /* Menyimpan matrix ke dalam file */
+        try{
+            FileWriter writer = new FileWriter(output);
+            for (int i = 0; i < this.row; i++){
+                for (int j = 0; j < this.col; j++){
+                    writer.write(Double.toString(this.contents[i][j]) + " ");
+                }
+                writer.write("\n");
+            }
+            System.out.println("File berhasil dibuat");
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void detToFile(File output, double det) {
+    /* Menyimpan hasil perhitungan determinan matriks ke dalam file */
+        try{
+            FileWriter writer = new FileWriter(output);
+            writer.write("Determinan = " + Double.toString(det) + "\n");
+            System.out.println("File berhasil dibuat");
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void saveToFile(File output, String s){
+    /* Menyimpan hasil lain-lain ke dalam file */
+        try{
+            FileWriter writer = new FileWriter(output);
+            writer.write(s + "\n");
+            System.out.println("File berhasil dibuat");
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     /* *** TEST *** */
     
     /* *** OPERATIONS *** */
-
+    double detCofactor(){
+    /* Mengembalikan nilai determinan yang diperoleh dengan cara ekspansi kofaktor */
+        if (this.row == 1){
+            return this.contents[0][0];
+        }
+        else{
+            double det = 0;
+            int a = 1;
+            for (int i = 0; i < this.row; i++) {
+                Matrix m1 = new Matrix(this.row - 1, this.col - 1);
+                for (int j = 0; j < m1.row; j++){
+                    for (int k = 0; k < m1.col; k++){
+                        if (k < i){
+                            m1.contents[j][k] = this.contents[j+1][k];
+                        }
+                        else{
+                            m1.contents[j][k] = this.contents[j+1][k+1];
+                        }
+                    }
+                }
+                det += a*this.contents[0][i]*m1.detCofactor();
+                a *= -1;
+            }
+            return det;
+        }
+    }
 
 }
 
